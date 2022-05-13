55aa5f54eb5ed8f3d782f08abd8e5269f74200ca
Mohit Kumar <89012914+mohitkumarg3@users.noreply.github.com>
2022-05-13 18:13:49 +0530

    Update variables.tf

--- aws_sns/variables.tf
+++ aws_sns/variables.tf
@@ -1,22 +1,19 @@
 variable "access_key" {
         description = "Access key to AWS console"
-        default     =    "<access_key>"
+        default     =    ""
+        type        =    string
 }
 variable "secret_key" {
         description = "Secret key to AWS console"
-        default     =    "<secret_key>"
+        default     =    ""
+        type        =    string
 }
 
 
 variable "sns_name" {
         description = "Name of the SNS Topic to be created"
-        default = "my_first_sns"
 }
 
-variable "account_id" {
-        description = "My Accout Number"
-        default = "<account_id>"
-}
 
 variable "region" {
     
