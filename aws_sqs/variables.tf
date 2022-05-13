0b6e6fee3ceb2382c2ec180f1d8992a2cb0d7b06
Mohit Kumar <89012914+mohitkumarg3@users.noreply.github.com>
2022-05-13 18:23:30 +0530

    Update variables.tf

--- aws_sqs/variables.tf
+++ aws_sqs/variables.tf
@@ -1,16 +1,17 @@
 variable "access_key" {
         description = "Access key to AWS console"
-        default     =    "<access_key>"
+        default     =    ""
+        type        =    string
 }
 variable "secret_key" {
         description = "Secret key to AWS console"
-        default     =    "<secret_key>"
+        default     =    ""
+        type        =    string
 }
 
 
 variable "sqs_name" {
         description = "Name of the sqs queue to be created"
-        default = "my-sqs"
 }
 
 variable "region" {
